1533c89e97f8689d900e5e4cc691daca047b3688
bergthor13 <bergthor13@ru.is>
2014-10-13 18:08:43 +0000

    Refactoring

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,25 +2,33 @@ package is.ru.stringcalculator;
 
 public class Calculator {
 	
-	public int add(String numbers) {
-		numbers = numbers.replaceAll("\\n", ",");
-		String[] commaArr = numbers.split(",");
-		int addedValue = 0;
-
-		for(int i = 0; i < commaArr.length; i++) {
-			if (!commaArr[i].equals("")) {
-				addedValue += toInt(commaArr[i]);
-			}
-		}
-
-		return addedValue;
-	}
-
 	private int toInt(String s) {
 		if(s.equals("")) {
 			return 0;
 		} else {
 			return Integer.parseInt(s);
 		}
+	}
+
+	private String normalizeDelimiters(String s) {
+		return s.replaceAll("\\n", ",");
+	}
+
+	private String[] splitIntegers(String s) {
+		return s.split(",");
+	}
+
+	public int add(String numbers) {
+		numbers = normalizeDelimiters(numbers);
+		String[] intArr = splitIntegers(numbers);
+		int addedValue = 0;
+
+		for(int i = 0; i < intArr.length; i++) {
+			if (!intArr[i].equals("")) {
+				addedValue += toInt(intArr[i]);
+			}
+		}
+
+		return addedValue;
 	}
 }
