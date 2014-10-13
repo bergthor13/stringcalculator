2c80b1fcaba29263c45173e73a74e0ebcf573dfb
bergthor13 <bergthor13@ru.is>
2014-10-13 17:20:51 +0000

    Refactoring by adding toInt function

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,14 +1,20 @@
 package is.ru.stringcalculator;
 
 public class Calculator {
+	
 	public int add(String numbers) {
 		String[] numberArr = numbers.split(",");
 		if(numberArr[0] == "") {
 			return 0;
 		}
 		if(numberArr.length == 2) {
-			return Integer.parseInt(numberArr[0])+Integer.parseInt(numberArr[1]);
+			return toInt(numberArr[0]) + toInt(numberArr[1]);
 		}
-		return Integer.parseInt(numberArr[0]);
+
+		return toInt(numberArr[0]);
+	}
+
+	private int toInt(String s) {
+		return Integer.parseInt(s);
 	}
 }
